7e24fd27bed37c13deb68f2e08cd225d40177556
SullyCB <Theepicsully@gmail.com>
2021-12-22 10:19:51 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,29 +8,36 @@ wss.on('listening', () => console.log('Backend online!'));
 wss.on('connection', client => {
     console.log('client connected!');
 
-    client.on('message', message => {
-        /*
-        {
-            username: string;
-            imageId: number;
-            message: string;
-        }
-        */
+    try {
+        client.on('message', message => {
+            /*
+            {
+                username: string;
+                imageId: number;
+                message: string;
+            }
+            */
+    
+           console.log(`Got message: ${message.toString('utf-8')}`);
+    
+           let data;
+    
+           try {
+                data = JSON.parse(message.toString('utf-8'));
+               
+           } catch (e) { return; } //epic validator
+    
+           if (data.username && data.message) {
+               console.log('sending to clients');
+               wss.clients.forEach(client => client.send(JSON.stringify(data)));
+           }
 
-       console.log(`Got message: ${message.toString('utf-8')}`);
+           client.on('error', () => {}); //zach proof code
+    
+        });
 
-       let data;
-
-       try {
-            data = JSON.parse(message.toString('utf-8'));
-           
-       } catch (e) { return; } //epic validator
-
-       if (data.username && data.message) {
-           console.log('sending to clients');
-           wss.clients.forEach(client => client.send(JSON.stringify(data)));
-       }
-
-    });
+    } catch (e) {
+        console.log(e);
+    }
 
 });
